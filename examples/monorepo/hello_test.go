c712ae8ea7150efe7260c2736499554340d00345
Leonidas Vrachnis <leo.al.vra@gmail.com>
2022-10-16 21:18:51 +0300

    fix: relative paths (#1)

--- examples/monorepo/hello_test.go
+++ examples/monorepo/hello_test.go
@@ -9,4 +9,5 @@ import (
 
 func TestHello(t *testing.T) {
 	require.Equal(t, "Hello world", monorepo.Hello("world"))
+	require.Equal(t, "Hello human", monorepo.Hello("human"))
 }
--- setup/project.go
+++ setup/project.go
@@ -1,16 +1,21 @@
 package setup
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"path"
+	"regexp"
 	"strings"
 	"text/template"
 
 	"github.com/rudderlabs/rudder-go-setup/files"
 	"golang.org/x/mod/modfile"
 )
+
+var emptyComment = regexp.MustCompile(`^\s*#\s*$`)
 
 type Project struct {
 	Name           string
@@ -44,8 +49,8 @@ func (p *Project) Detect() error {
 		return fmt.Errorf("failed to parse go.mod in working directory: %w", err)
 	}
 
-	p.ProjectRelPath = path.Join(".", path.Clean(strings.TrimPrefix(wd, p.RepositoryRoot)))
-	if p.ProjectRelPath != "." {
+	p.ProjectRelPath = strings.TrimPrefix(strings.TrimPrefix(wd, p.RepositoryRoot), "/")
+	if p.ProjectRelPath != "" {
 		p.Nested = true
 	}
 
@@ -103,14 +108,43 @@ func (p *Project) Init() error {
 		if err != nil {
 			return err
 		}
+		// filter for empty comments, template artifacts
+		ff := filter(f, emptyComment.Match)
 
-		if err := pt.ExecuteTemplate(f, src, p); err != nil {
+		if err := pt.ExecuteTemplate(ff, src, p); err != nil {
 			return err
 		}
 
-		if err := f.Close(); err != nil {
+		if err := ff.Close(); err != nil {
 			return err
 		}
 	}
 	return nil
+}
+
+func filter(output io.Writer, fn func(line []byte) bool) io.WriteCloser {
+	r, w := io.Pipe()
+
+	go func() {
+		s := bufio.NewScanner(r)
+		for s.Scan() {
+			if fn(s.Bytes()) {
+				continue
+			}
+
+			_, err := output.Write(s.Bytes())
+			if err != nil {
+				w.CloseWithError(err)
+				return
+			}
+
+			_, err = output.Write([]byte{'\n'})
+			if err != nil {
+				w.CloseWithError(err)
+				return
+			}
+		}
+		w.CloseWithError(s.Err())
+	}()
+	return w
 }
